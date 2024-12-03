adafe9e167e80c90dba98dd85a8692ed459397bb
mykola.sharshov <mykola.sharshov@oracle.com>
2024-12-03 13:03:22 +0200

    cleanup

--- 3/solution.js
+++ 3/solution.js
@@ -6,7 +6,7 @@ function process(input) {
         .match(/mul\(\d{1,3},\d{1,3}\)/g)
         .map(mulString => mulString.match(/\d{1,3}/g))
         .map(([a, b]) => +a * +b)
-        .reduce((sum, x) => sum + x, 0)
+        .reduce((a, b) => a + b)
 }
 
 function solution1() {
@@ -17,7 +17,7 @@ function solution2() {
     let input = getInput();
     
     let toDo = input.split('do()')
-        .map(doStr => doStr.split("don\'t()")[0])
+        .map(doStr => doStr.split("don't()")[0])
         .join('');
     
     console.log(process(toDo));
@@ -27,7 +27,7 @@ function getInput() {
     try {
         return require('node:fs')
             .readFileSync('./input', 'utf8')
-            .trim('\n');            
+            .trim();            
     } catch (err) {
         console.error(err);
     }
