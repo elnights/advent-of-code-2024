f590a6e0132a98550db2b28a32f461edb4fdd1d4
elnights <elnights@gmail.com>
2024-12-07 13:03:48 +0200

    With precedence

--- 7/solution.js
+++ 7/solution.js
@@ -3,9 +3,26 @@ const { log } = require('node:console');
 solution1();
 
 function solution1() {
-    const lines = getInput();
+    function applyOperators(str, opBitbap) {
+        const getOp = opIndex => 1 << opIndex & opBitbap ? '+' : '*';
+        let currOpIndex = 0;
+        return str.replace(/\s+/g, () => getOp(currOpIndex++));
+    }
 
-    log(lines)
+    const result = getInput().reduce((result, [value, str]) => {
+        const numbers = str.split(' ');
+        const valueNum = +value;
+        const operatorsBitmap = 2 ** (numbers.length - 1) - 1;
+        // Loop through all operator combinations
+        for (let i = operatorsBitmap; i >= 0; i--) {
+            let exp = applyOperators(str, i)
+            //log(exp)
+            if (eval(exp) === valueNum) return result + valueNum;
+        }
+        return result;
+    }, 0);
+
+    log(result)
 }
 
 function getInput() { 
