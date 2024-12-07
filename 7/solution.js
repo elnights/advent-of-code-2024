a0159db2d4f6089b1fb3fd987d4b08ce028cc125
elnights <elnights@gmail.com>
2024-12-07 13:31:16 +0200

    Day 7

--- 7/solution.js
+++ 7/solution.js
@@ -1,33 +1,68 @@
 const { log } = require('node:console');
 
 solution1();
+solution2();
 
 function solution1() {
-    function applyOperators(str, opBitbap) {
-        const getOp = opIndex => 1 << opIndex & opBitbap ? '+' : '*';
+    function applyOperators(numbers, opBitbap) {
         let currOpIndex = 0;
-        return str.replace(/\s+/g, () => getOp(currOpIndex++));
+        return numbers.reduce((a, b) => 1 << currOpIndex++ & opBitbap ? a + b : a * b);
     }
 
     const result = getInput().reduce((result, [value, str]) => {
-        const numbers = str.split(' ');
+        const numbers = str.split(' ').map(Number);
         const valueNum = +value;
         const operatorsBitmap = 2 ** (numbers.length - 1) - 1;
         // Loop through all operator combinations
         for (let i = operatorsBitmap; i >= 0; i--) {
-            let exp = applyOperators(str, i)
-            //log(exp)
-            if (eval(exp) === valueNum) return result + valueNum;
+            if (applyOperators(numbers, i) === valueNum) return result + valueNum;
         }
         return result;
     }, 0);
 
-    log(result)
+    log(result);
+}
+
+function solution2() {
+    function* combinations(n, symbols) {
+        const max = Math.pow(symbols.length, n); // Total number of combinations
+    
+        for (let i = 0; i < max; i++) {
+            // Convert number to a base-N string and pad with zeros to length n
+            const baseString = i.toString(symbols.length).padStart(n, "0");
+    
+            // Map digits from base-0 (0,1,2) to symbols
+            yield [...baseString].map(d => symbols[d]);
+        }
+    }
+
+    function applyOperators(numbers, operators) {
+        let currOpIndex = 0;
+        return numbers.reduce((a, b) => {
+            switch(operators[currOpIndex++]) {
+                case '|': return +(a.toString() + b.toString());
+                case '+': return a + b;
+                case '*': return a * b;
+            }
+        });
+    }
+
+    const result = getInput().reduce((result, [value, str]) => {
+        const numbers = str.split(' ').map(Number);
+        const valueNum = +value;
+
+        for (const combination of combinations(numbers.length - 1, ['+', '*', '|'])) 
+            if (applyOperators(numbers, combination) === valueNum) return result + valueNum;
+        
+        return result;
+    }, 0);
+
+    log(result);
 }
 
 function getInput() { 
     return require('node:fs')
-        .readFileSync('./test_input', 'utf8')
+        .readFileSync('./input', 'utf8')
         .split('\n')
         .filter(s => s.trim())
         .map(s => s.split(': '));
